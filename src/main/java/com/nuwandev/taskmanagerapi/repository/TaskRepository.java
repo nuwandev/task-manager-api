246a8ba15bb65f29284680124ce2cb1628715154
Theekshana Nuwan <theekshanabro40@gmail.com>
2026-06-01 13:04:13 +0530

    feat: implement createTask method in TaskServiceImpl and add save method to TaskRepository

--- src/main/java/com/nuwandev/taskmanagerapi/repository/TaskRepository.java
+++ src/main/java/com/nuwandev/taskmanagerapi/repository/TaskRepository.java
@@ -1,4 +1,9 @@
 package com.nuwandev.taskmanagerapi.repository;
 
+import com.nuwandev.taskmanagerapi.entity.Task;
+
+import java.util.UUID;
+
 public interface TaskRepository {
+    UUID save(Task task);
 }
--- src/main/java/com/nuwandev/taskmanagerapi/service/impl/TaskServiceImpl.java
+++ src/main/java/com/nuwandev/taskmanagerapi/service/impl/TaskServiceImpl.java
@@ -4,28 +4,49 @@ import com.nuwandev.taskmanagerapi.dto.request.CreateTaskRequest;
 import com.nuwandev.taskmanagerapi.dto.request.UpdateTaskRequest;
 import com.nuwandev.taskmanagerapi.dto.response.TaskPageResponse;
 import com.nuwandev.taskmanagerapi.dto.response.TaskResponse;
+import com.nuwandev.taskmanagerapi.entity.Task;
 import com.nuwandev.taskmanagerapi.enums.Priority;
 import com.nuwandev.taskmanagerapi.enums.Status;
+import com.nuwandev.taskmanagerapi.repository.TaskRepository;
 import com.nuwandev.taskmanagerapi.service.TaskService;
 import org.springframework.stereotype.Service;
 
+import java.time.OffsetDateTime;
 import java.util.UUID;
 
 @Service
 public class TaskServiceImpl implements TaskService {
+
+    final private TaskRepository taskRepository;
+
+    public TaskServiceImpl(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
     @Override
     public UUID createTask(CreateTaskRequest request) {
-        return null;
+        Task task = new Task();
+
+        task.setId(UUID.randomUUID());
+        task.setTitle(request.title());
+        task.setDescription(request.description());
+        task.setStatus(request.status() != null ? request.status() : Status.TODO);
+        task.setPriority(request.priority() != null ? request.priority() : Priority.MEDIUM);
+        task.setDueDate(request.dueDate());
+        task.setCreatedAt(OffsetDateTime.now());
+        task.setUpdatedAt(OffsetDateTime.now());
+
+        return taskRepository.save(task);
     }
 
     @Override
     public TaskResponse getTaskById(UUID id) {
-        return null;
+
     }
 
     @Override
     public TaskResponse updateTask(UUID id, UpdateTaskRequest request) {
-        return null;
+
     }
 
     @Override
@@ -35,6 +56,6 @@ public class TaskServiceImpl implements TaskService {
 
     @Override
     public TaskPageResponse listTasks(int page, int size, String search, Status status, Priority priority, String sort) {
-        return null;
+
     }
 }
