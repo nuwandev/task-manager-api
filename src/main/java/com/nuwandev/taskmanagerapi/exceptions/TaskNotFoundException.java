d15c80d7f0aa3171e33f2fa7b9cf3e60ce59b6e1
Theekshana Nuwan <theekshanabro40@gmail.com>
2026-06-01 13:42:10 +0530

    feat: add default message to TaskNotFoundException constructor

--- src/main/java/com/nuwandev/taskmanagerapi/exceptions/TaskNotFoundException.java
+++ src/main/java/com/nuwandev/taskmanagerapi/exceptions/TaskNotFoundException.java
@@ -1,4 +1,7 @@
 package com.nuwandev.taskmanagerapi.exceptions;
 
 public class TaskNotFoundException extends RuntimeException {
+    public TaskNotFoundException() {
+        super("Task not found");
+    }
 }
