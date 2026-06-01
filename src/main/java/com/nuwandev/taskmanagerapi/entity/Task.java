48c25cea4299404fb9c0b3cc6c5b6edd31061a44
Theekshana Nuwan <theekshanabro40@gmail.com>
2026-06-01 12:47:57 +0530

    feat: add getters and setters for Task properties

--- src/main/java/com/nuwandev/taskmanagerapi/entity/Task.java
+++ src/main/java/com/nuwandev/taskmanagerapi/entity/Task.java
@@ -15,4 +15,81 @@ public class Task {
     private Timestamp dueDate;
     private Timestamp createdAt;
     private Timestamp updatedAt;
+
+    public Task() {}
+
+    public Task(UUID id, String title, String description, Status status, Priority priority, Timestamp dueDate, Timestamp createdAt, Timestamp updatedAt) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.status = status;
+        this.priority = priority;
+        this.dueDate = dueDate;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public Priority getPriority() {
+        return priority;
+    }
+
+    public Timestamp getDueDate() {
+        return dueDate;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public Timestamp getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public void setPriority(Priority priority) {
+        this.priority = priority;
+    }
+
+    public void setDueDate(Timestamp dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public void setUpdatedAt(Timestamp updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
